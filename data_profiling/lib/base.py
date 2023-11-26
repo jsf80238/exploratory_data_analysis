a599189088506d2f2868c84293c35bae2efb5988
Jason Friedman <jsf80238@gmail.com>
2023-11-25 17:37:37 -0700

    .

--- data_profiling/lib/base.py
+++ data_profiling/lib/base.py
@@ -30,9 +30,9 @@ if major < min_major or minor < min_minor:
 
 old_factory = logging.getLogRecordFactory()
 
-
 class Config:
     PRIMARY_CONFIG_FILE = "config.yaml"
+    CONFIG_DIR = Path(__file__).parent.parent.parent / "config"
 
     @classmethod
     def get_config(cls, file_name: str = PRIMARY_CONFIG_FILE) -> dict:
@@ -41,26 +41,31 @@ class Config:
         :param file_name: file within the configuration directory
         :return: the configuration corresponding to that file
         """
-        config_dir = Path(__file__).parent.parent.parent / "config"
         if not file_name.lower().endswith(".yaml"):
             file_name += ".yaml"
-        config_file = config_dir / file_name
+        config_file = cls.CONFIG_DIR / file_name
         text = open(config_file).read()
         return load(text, Loader=Loader)
 
 
 class C(enum.StrEnum):
     BLACK_SQUARE = unicodedata.lookup("BLACK SQUARE")  # ■, x25A0
-    CSV_EXTENSION = ".csv"
-    SQL_EXTENSION = ".sql"
-    EXCEL_EXTENSION = ".xlsx"
     CHAR = "CHAR"
+    CLASSPATH = "CLASSPATH"
+    CLASS_NAME = "class_name"
+    CONNECTION_STRING = "connection_string"
+    CSV_EXTENSION = ".csv"
+    DATABASE = "database"
     DATE = "DATE"
     DECIMAL = "DECIMAL"
+    EXCEL_EXTENSION = ".xlsx"
     FLOAT = "FLOAT"
+    JAR = "jar"
+    JDBC = "jdbc"
     NUMBER = "NUMBER"
+    PORT_NUMBER = "port_number"
+    SQL_EXTENSION = ".sql"
     VARCHAR = "VARCHAR"
-    DATABASE = "database"
 
 
 config_dict = Config.get_config()
@@ -153,15 +158,27 @@ class Database:
         if not cls.__instance:
             cls.logger.info(f"Connecting to '{database_name} as {user_name}' ...")
             # Determine database type based on port number
-            dbtype_dict = config_dict["jdbc"]["port_number"]
+            dbtype_dict = config_dict[C.JDBC][C.PORT_NUMBER]
             for database_type_name, assigned_port in dbtype_dict.items():
                 if assigned_port == port_number:
                     break
             else:
                 raise Exception(f"I don't know what kind of database listens on port {port_number}.")
-            # database_connection = jdbc.connect(class_name, cls.connect_string, [cls.user_name, cls.password], jdbc_path)
+            classpath = Path(config_dict[C.JDBC][C.JAR][database_type_name])
+            absolute_classpath = Path(__file__).parent.parent / classpath
+            os.environ[C.CLASSPATH] = os.environ.get(C.CLASSPATH, "") + path_separator + classpath.as_posix()
+            connection_string = config_dict[C.JDBC][C.CONNECTION_STRING][database_type_name]
+            connection_string = connection_string.replace("put_host_name_here", host_name)
+            connection_string = connection_string.replace("put_port_number_here", str(port_number))
+            connection_string = connection_string.replace("put_database_name_here", database_name)
+            cls.database_connection = jdbc.connect(config_dict[C.JDBC][C.CLASS_NAME][database_type_name],
+                                      connection_string,
+                                      [user_name, password],
+                                      absolute_classpath.as_posix())
             cls.logger.info("... connected.")
-            # database_connection.jconn.setAutoCommit(auto_commit)
+            cls.database_connection.jconn.setAutoCommit(auto_commit)
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
 
     @classmethod
     def get_connection(cls) -> jdbc.Connection:
@@ -270,12 +287,13 @@ class Database:
 
 
 connection = Database(
-    host_name="x",
-    port_number=1,
-    database_name="y",
+    host_name="localhost",
+    port_number=1433,
+    database_name="master",
     user_name="sa",
     password="!1Jkrvhmhzyjwc"
 ).get_connection()
+print(connection)
 exit()
 
 def dedent_sql(s):
